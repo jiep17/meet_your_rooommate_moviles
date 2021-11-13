749abe9bf7184568564aea72143250f561b24739
renato-17 <renato.avela.17@gmail.com>
2021-11-12 19:28:54 -0500

    gitignore

--- MeetYourRoommate/app/src/main/java/pe/edu/upc/meetyourroommate/data/remote/responses/LessorResponse.kt
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.meetyourroommate.data.remote.responses
-
-import com.google.gson.annotations.SerializedName
-import pe.edu.upc.meetyourroommate.data.model.Lessor
-
-class LessorResponse(
-    @SerializedName("content")
-    val lessors: List<Lessor>
-)
--- MeetYourRoommate/app/src/main/java/pe/edu/upc/meetyourroommate/data/remote/responses/PropertyResponse.kt
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.meetyourroommate.data.remote.responses
-
-import com.google.gson.annotations.SerializedName
-import pe.edu.upc.meetyourroommate.data.model.Property
-
-class PropertyResponse(
-    @SerializedName("content")
-    val properties: List<Property>
-)
